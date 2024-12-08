e3d9ad76d4fb835b31f8363e7b7e42b397c981aa
Georgi Brankovanov <georgi.brankovanov@gmail.com>
2024-12-08 11:43:39 +0200

    Add constants about the new sections logic

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,14 @@
 export const constants = {
+  // Header ids
+  titleId: "title",
+  logoId: "header-logo",
+  metaTagId: '[name="apple-mobile-web-app-title"]',
+
+  // Side navigation ids 
+  sideNavigationId: '[id="navigation-side-menu"]',
+  navItemId: '[id="navigation-list"]',
+  languageSelectId: '[id="language-select"]',
+
   // Carousel ids
   carouselControlsId: '[id="carousel-controls"]',
   slidesWrapperId: '[id="slides-wrapper"]',
@@ -8,6 +18,7 @@ export const constants = {
   aboutUsImageId: '[id="about-us-image"]',
   aboutUsTitleId: '[id="about-us-title"]',
   aboutUsBodyId: '[id="about-us-body"]',
+  aboutUsNavigationId: "#about-us",
 
   // Products ids
   productsSectionId: '[id="products-section-container"]',
@@ -16,12 +27,14 @@ export const constants = {
   productTitleId: '[id="product-title"]',
   productContentId: '[id="product-content"]',
   productImageId: '[id="product-image"]',
+  productsNavigationId: "#products-section",
 
   // Services ids
   servicesSectionId: '[id="services-wrapper"]',
   servicesTitleId: '[id="services-title"]',
   servicesTabWrapperId: '[id="services-tab-navigation-wrapper"]',
   servicesContentWrapperId: '[id="services-tab-content-wrapper"]',
+  servicesNavigationId: "#services",
 
   //Our team ids
   ourTeamSectionId: '[id="out-team-wrapper"]',
@@ -31,6 +44,7 @@ export const constants = {
   teamMemberNameId: '[id="team-member-name"]',
   teamMemberPositionId: '[id="team-member-position"]',
   teamBioIdentifier: 'data-bio',
+  teamNavigationId: "#our-team",
 
   //Testimonials
   testimonialsSectionId: '[id="testimonials-wrapper"]',
@@ -39,6 +53,26 @@ export const constants = {
   testimonialContentId: '[id="testimonial-content"]',
   testimonialCredentialsId: '[id="testimonial-credentials"]',
   testimonialDateId: '[id="testimonial-date"]',
+  testimonialsNavigationId: "#testimonials",
+
+  //Contacts 
+  contactsNavigationId: "#contacts",
+  contactsSectionId: '[id="contact-section-container"]',
+  contactsSectionTitleId: '[id="contacts-section-title"]',
+  inputName: '[id="name-input"]',
+  inputEmail: '[id="email-input"]',
+  inputPhone: '[id="phone-input"]',
+  inputSelect: '[id="select-input"]',
+  inputTextArea: '[id="text-area-input"]',
+  phoneNumber: '[id="phone-number"]',
+  phoneLinkPrefix: 'tel:',
+  emailAddress: '[id="email-address"]',
+  mailLinkPrefix: 'mailto:',
+  address: '[id="address"]',
+  sendButtonId: '[id="submit-button"]',
+
+  //Footer
+  footerContentId: '[id="footer-wrapper"]',
 
   //Modals
   modalLabelDataAttribute: '[data-modal-label="modalLabel"]',
@@ -70,7 +104,9 @@ export const constants = {
   button: 'button',
 
   //Language
-  defaultLanguage: 'bg',
+  bulgarianLanguage: 'bg',
+  englishLanguage: 'en',
+  germanLanguage: 'de',
 
   // Cookies
   cookieTypes: {
@@ -84,5 +120,7 @@ export const constants = {
 
   //Error
   httpError: 'HTTP error! Status: ',
-  fetchingError: 'Error fetching JSON:'
+  fetchingError: 'Error fetching JSON:',
+
+  yearCreated: 2024
 }
